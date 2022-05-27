6ae267bf44520de6c886b0a5f15b50cd5cc64e7c
dsound-zz <demiansims@gmail.com>
2022-05-26 21:37:47 -0400

    change layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { GlobalStyles } from "./global";
 import { colors } from "./colors";
-import Avatar from "./assets/avatar.png";
+import avatar from "./assets/avatar.png";
 import TypeScriptLogo from "./assets/typescript.png";
 import ReactLogo from "./assets/react.png";
 import RailsLogo from "./assets/rails2.png";
@@ -14,46 +14,18 @@ import ResumeLogo from "./assets/resume.png";
 import ApolloLogo from "./assets/apollo.png";
 
 const Container = styled.div`
-  heigt: 100vh;
-  width: 100vw;
-  margin: 0;
-  display: flex;
-  flex-direction: row;
-`;
-
-const Main = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`;
-
-const ContentWrapper = styled.div`
-  border: solid 1px ${colors.lightCream};
-  padding: 50px 50px;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  width: 50%;
+  color: white;
 `;
 
-const Name = styled.h2`
-  color: ${colors.lightCream};
-  flex: 3;
-  margin-bottom: 20px;
-`;
-
-const Text = styled.div`
-  color: ${colors.lightCream};
-  flex: 1;
-`;
-
-const Navigation = styled.div`
+const NavBar = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 10px;
+  padding: 50px;
   width: calc(100% - 20px);
 
   @media (max-width: 768px) {
@@ -71,11 +43,21 @@ const LeftSideNav = styled.div`
   }
 `;
 
-const NavItem = styled.button`
-  border: 1px solid ${colors.lightCream};
-  color: ${colors.lightCream};
+const RightSideNav = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const NavItem = styled.a`
+  border: ${colors.lightCream} 1px solid;
+  color: ${colors.lightCream}
   font-size: 20px;
-  margin: 0 40px 20px 0;
+  margin: 0px 50px 20px 0;
   padding: 10px;
 
   &:hover {
@@ -83,33 +65,32 @@ const NavItem = styled.button`
   }
 `;
 
-const Logo = styled.img`
-  margin-bottom: 20px;
-  order: 2;
-  width: 200px;
-  height: 200px;
+const Avatar = styled.img`
+  width: 100px;
+  height: 100px;
+`;
 
-  @media (max-width: 768px) {
-    order: 1;
+const Content = styled.div`
+  border: white solid 1px;
+  flex: 1;
+  width: calc(100% - 20px);
+`;
+
+const Text = styled.div`
+  padding: 5em;
+
+  h1 {
+    color: ${colors.lightCream};
+    letter-spacing: 0.2rem;
   }
 `;
 
-const SkillLogo = styled.img`
-  margin-top: 50%;
-  margin-bottom: 20px;
-  order: 2;
-  width: 40px;
-  height: 40px;
-
-  @media (max-width: 768px) {
-    order: 1;
-  }
-`;
-const Aside = styled.div`
-  height: 100vh;
-  width: 20vw;
-  background-color: #b2dfdb;
-  padding: 1rem;
+const Skills = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  border: white solid 1px;
+  height: 20vh;
 `;
 
 function App() {
@@ -117,28 +98,29 @@ function App() {
     <>
       <GlobalStyles />
       <Container>
-        <Aside>
-          <SkillLogo alt="typescriptLogo" src={TypeScriptLogo} />
-          <SkillLogo alt="reactLogo" src={ReactLogo} />
-          <SkillLogo alt="apolloLogo" src={ApolloLogo} />
-          <SkillLogo alt="rubyLogo" src={RubyLogo} />
-          <SkillLogo alt="railsLogo" src={RailsLogo} />
-        </Aside>
-        <Navigation>
+        <NavBar>
           <LeftSideNav>
             <NavItem>Github</NavItem>
-            <NavItem>Resume</NavItem>
+            <NavItem>Linkedin</NavItem>
             <NavItem>Projects</NavItem>
+            <NavItem>Resume</NavItem>
           </LeftSideNav>
-          <Logo alt="Logo" src={Avatar} />
-        </Navigation>
-
-        <Main>
-          <ContentWrapper>
-            <Name>Demian Sims</Name>
-            <Text>Writing beautiful, clean and effeicient code</Text>
-          </ContentWrapper>
-        </Main>
+          <RightSideNav>
+            <Avatar src={avatar} />
+          </RightSideNav>
+        </NavBar>
+        <Content>
+          <Text>
+            <h1>Demian Sims</h1>
+          </Text>
+          <Skills>
+            <h3>ReactJS</h3>
+            <h3>React Native</h3>
+            <h3>Typescript</h3>
+            <h3>Rails</h3>
+            <h3>Accessibility</h3>
+          </Skills>
+        </Content>
       </Container>
     </>
   );
